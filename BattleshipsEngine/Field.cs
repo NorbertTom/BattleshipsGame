85e16a130abcbe29dd6eae76b340db8fbb427b7d
NorTom <n0r@o2.pl>
2021-04-13 09:20:51 +0200

    Added Field class and tests

---
+++ BattleshipsEngine/Field.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BattleshipsEngine
+{
+    class Field
+    {
+        public Field()
+        { }
+
+        public bool IfShot() { return wasShot; }
+        public void PlaceShip(IShip ship) { this.ship = ship; }
+
+        public IShip Shoot()
+        {
+            if (wasShot)
+            {
+                throw new Exception("Field already shot");
+            }
+
+            wasShot = true;
+            return ship;
+        }
+
+        private bool wasShot = false;
+        private IShip ship = null;
+    }
+}
---
+++ BattleshipsEngineTests/FieldTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using Moq;
+using BattleshipsEngine;
+
+namespace BattleshipsEngineTests
+{
+    public class FieldTests
+    {
+        [Fact]
+        public void IfFieldWasShot()
+        {
+            var field = new Field();
+            
+            Assert.False(field.IfShot());
+        }
+
+        [Fact]
+        public void ShootEmptyField()
+        {
+            var field = new Field();
+
+            Assert.False(field.IfShot());
+            Assert.Null(field.Shoot());
+            Assert.True(field.IfShot());
+        }
+
+        [Fact]
+        public void PlaceShipAndShootIt()
+        {
+            var mockShip = new Mock<IShip>();
+            var field = new Field();
+            
+            field.PlaceShip(mockShip.Object);
+         
+            Assert.False(field.IfShot());
+            Assert.Equal(mockShip.Object, field.Shoot());
+            Assert.True(field.IfShot());
+        }
+    }
+}
